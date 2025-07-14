5b95b646197bf100c9b72df8c9574737600a56a1
Sam <74906440+anonymous799@users.noreply.github.com>
2025-07-14 21:59:46 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -9,10 +9,10 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const supportServerLink = "";
+            const githubLink = "";
+            const replitLink = "";
+            const youtubeLink = "";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
@@ -27,7 +27,7 @@ module.exports = {
                     .replace("{replitLink}", replitLink)
                     .replace("{youtubeLink}", youtubeLink)
                 )
-                .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+                .setImage('https://cdn.discordapp.com/attachments/1103645060081397793/1394033884898918473/ig.S_a_m.png?ex=6875ff7a&is=6874adfa&hm=2ed83200ae12a175449e5c6b10d5aec208fd05961fbfbece987d02f4093ea4ec&')
                 .setTimestamp();
 
             await interaction.reply({ embeds: [embed] });
