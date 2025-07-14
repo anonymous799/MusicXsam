e517f902004381d61d3ef84394e599dedf14baec
Sam <74906440+anonymous799@users.noreply.github.com>
2025-07-14 22:02:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://razexelite11:spekbot@spek.dl9mwva.mongodb.net",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
